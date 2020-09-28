df188263b9a1627d22562a7f9022a8aeb07b684b
Alex Meza <alex@mezainnovations.com>
2020-09-28 16:57:11 -0500

    Streamline info from web playback

--- search/views.py
+++ search/views.py
@@ -11,10 +11,8 @@ def search(request):
     if request.user.is_authenticated:
         spotify_user = request.user.social_auth.get(provider='spotify')
         sp = spotipy.Spotify(auth=spotify_user.extra_data["access_token"])
-        current_user = json.dumps(sp.current_user())
-        current_song = json.dumps(sp.current_user_playing_track())
+        current_user = sp.current_user()
         extra = {}
         extra["current_user"] = current_user
-        extra["current_song"] = current_song
         extra["access_token"] = spotify_user.extra_data["access_token"]
     return render(request, 'login/login.html', {'extra': extra})
